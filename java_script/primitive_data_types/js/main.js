3a61b5c64f2f690c8a92c78f6fd67e59a6f7729c
Bo Doi <chulong_t65@hus.edu.vn>
2024-12-24 20:46:30 +0700

    JavaScript _ Primitive Data Types

---
+++ java_script/primitive_data_types/js/main.js
@@ -0,0 +1,34 @@
+let myString = `abc`;
+console.log(myString);
+
+// Kiểm tra kiểu dữ liệu của 1 biến
+console.log(typeof myString);
+
+// Kiểu number
+let soNguyen = 10;
+let soThuc = 3.14;
+
+console.log(soNguyen);
+console.log(soThuc);
+
+console.log(typeof soNguyen);
+console.log(typeof soThuc);
+
+// Kiểu boolean
+let check = true;
+
+console.log(typeof check);
+
+// Kiểu undefined
+let diemToan;
+
+console.log(typeof diemToan);
+
+// Kiểu null
+let connect = null;
+
+console.log(typeof connect);
+
+// Xuất số nguyên an toàn tối đa / tối thiểu
+console.log("Số nguyên an toàn tối đa: " + Number.MAX_SAFE_INTEGER);
+console.log("Số nguyên an toàn tối thiểu: ", Number.MIN_SAFE_INTEGER);
