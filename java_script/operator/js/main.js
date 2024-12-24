0dcc2f4c415f33157f175421bf8f2f68b4cdcce5
Bo Doi <chulong_t65@hus.edu.vn>
2024-12-24 23:44:16 +0700

    JavaScript _ Operator

---
+++ java_script/operator/js/main.js
@@ -0,0 +1,15 @@
+let a = 7;
+let b = 2;
+
+let tong = a + b;
+let hieu = a - b;
+let tich = a * b;
+let thuong = a / b;
+let soDu = a % b;
+
+// Xuất kết quả
+console.log(`Tổng 2 số: %s`, tong);
+console.log(`Hiệu 2 số: %s`, hieu);
+console.log(`Tích 2 số: %s`, tich);
+console.log(`Thương 2 số: %s`, thuong);
+console.log(`Số dư của phép chia a cho b là: %s`, soDu);
