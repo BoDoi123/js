45cbcf276f2ca44152e6ef8ad69a5dc7461bf4b1
Bo Doi <chulong_t65@hus.edu.vn>
2024-12-24 19:23:23 +0700

    JavaScript _ Variable

---
+++ java_script/variable/exercise1/js/main.js
@@ -0,0 +1,4 @@
+var yourName = prompt("Mời cụ nhập tên: ");
+
+console.log("Tên của bạn là: " + yourName);
+console.log("Chào mừng " + yourName + " đến với trang Tuhoc.cc!");
---
+++ java_script/variable/js/main.js
@@ -0,0 +1,28 @@
+// Hàm hiển thị hộp thoại lên trên trình duyệt cho người dùng
+alert("Xin chao");
+
+// Hiển thị hộp thoại nhập liệu cho người dùng
+prompt("Mời nhập vào tên của bạn: ");
+
+// Ví dụ về khai báo biến
+var myName;
+let yourName;
+// const doSoi;
+
+// Khởi tạo biến
+var diemToan = 10;
+const doSoi = 100;
+let firstName = "Alice";
+
+// Xuất giá trị của biến
+console.log(diemToan);
+console.log("Điểm Toán của bạn = " + diemToan + " điểm");
+console.log(doSoi);
+console.log(firstName);
+
+// Thử thay đổi giá trị của biến
+diemToan = 5;
+console.log("Điểm Toán của bạn = " + diemToan + " điểm");
+
+firstName = "Jacky";
+console.log(firstName);
