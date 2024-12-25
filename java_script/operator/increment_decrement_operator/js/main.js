2c59d5f1db76199794a021d33d4a3afb2afdf790
Bo Doi <chulong_t65@hus.edu.vn>
2024-12-25 16:15:58 +0700

    JavaScript _ Increment / Decrement Operators

---
+++ java_script/operator/increment_decrement_operator/js/main.js
@@ -0,0 +1,24 @@
+let a = 99;
+let b = 10;
+let c = 77;
+let d = 88;
+
+a++;
+b--;
+++c;
+--d;
+
+// Xuất kết quả
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+
+// 2. Trường hợp biểu thức phức tạp
+let x = 1;
+let y = 2;
+let z = x++ - ++y + 1;
+
+console.log(x);
+console.log(y);
+console.log(z);
