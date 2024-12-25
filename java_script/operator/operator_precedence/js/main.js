9ceabac21c09fd4d9673e9ffa75e51dbcf3ccca7
Bo Doi <chulong_t65@hus.edu.vn>
2024-12-25 16:16:24 +0700

    JavaScript _ Operator Precedence

---
+++ java_script/operator/operator_precedence/js/main.js
@@ -0,0 +1,13 @@
+// Ưu tiên ()
+let diemToan = 9.5;
+let diemVan = 8.75;
+
+let dtb = (diemToan + diemVan) / 2;
+
+console.log(dtb);
+
+// Right To Left (Thứ tự ưu tiên trên cùng 1 dòng)
+let x = (y = 25);
+
+// Left To Right
+let c = 25 - 7 + 8 - 1;
