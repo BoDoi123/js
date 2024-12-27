8e9d201f5e82c56257359ad6d45eb72ab5178813
Bo Doi <chulong_t65@hus.edu.vn>
2024-12-27 23:07:26 +0700

    JavaScript _ Ternary Operator

---
+++ java_script/ternary_operator/js/main.js
@@ -0,0 +1,11 @@
+let number = 10;
+
+let message = number >= 0 ? "So duong" : "So am";
+
+console.log(message);
+
+number = 11;
+
+message = number % 2 === 0 ? "So chan" : "So le";
+
+console.log(message);
