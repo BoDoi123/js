ceeb566a0ea236c658bb4b9a2da570b42de83b04
Bo Doi <chulong_t65@hus.edu.vn>
2024-12-30 19:31:06 +0700

    JavaScript _ Concat & toLower & toUpper & CharAt

---
+++ java_script/string/concat_charAt_toUpperCase_toLowerCase/js/main.js
@@ -0,0 +1,20 @@
+// let newString = str1.concat(str2, str3, ..., strN);
+let firstName = "John";
+let lastName = "Doe";
+
+let fullName = firstName.concat(" ", lastName);
+
+console.log(fullName);
+
+// toUpperCase()
+let s18 = "Chao cac ban, toi hOC TAI Tuhoc.cc";
+
+console.log(s18.toUpperCase);
+
+// toLowerCase()
+console.log(s18.toLowerCase);
+
+// charAt(index)
+let s19 = "Hello";
+
+console.log(s19.charAt(2));
