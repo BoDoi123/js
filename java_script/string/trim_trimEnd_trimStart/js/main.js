160f1b48695984786ec4d425073e1005f25c1666
Bo Doi <chulong_t65@hus.edu.vn>
2024-12-30 19:21:29 +0700

    JavaScript _ String JS (Trim, TrimEnd, TrimStart)

---
+++ java_script/string/trim_trimEnd_trimStart/js/main.js
@@ -0,0 +1,24 @@
+// string.trim()
+let s11 = "          He              llo          World           ";
+
+console.log(s11.length);
+
+let s12 = s11.trim();
+
+console.log(s12.length);
+
+console.log(s12);
+
+// string.trimEnd()
+let s13 = "          He              llo          World           ";
+
+let s14 = s13.trimEnd();
+
+console.log(s14);
+
+// string.trimStart()
+let s15 = "          He              llo          World           ";
+
+let s16 = s15.trimStart();
+
+console.log(s16);
